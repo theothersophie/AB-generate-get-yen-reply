a52156a25ec95034dc37896463edbcb907989360
Sophia <war.flower@hotmail.com>
2018-05-08 16:03:27 -0400

    metadata

--- AB-generate-get-yen-reply.user.js
+++ AB-generate-get-yen-reply.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Get yen for raw manga and novels reply generator
 // @author      theothersophie
-// @include     *animebytes.tv/alltorrents.php?type=uploaded*
+// @include     *animebytes.tv/alltorrents.php*
 // @namespace   https://github.com/theothersophie/AB-generate-get-yen-reply/raw/master/AB-generate-get-yen-reply.user.js
 // @downloadURL https://github.com/theothersophie/AB-generate-get-yen-reply/raw/master/AB-generate-get-yen-reply.user.js
 // @require     https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js
