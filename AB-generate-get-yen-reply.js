d6c39da8e0b579010d4f033931f4d272ca01085a
Sophia <war.flower@hotmail.com>
2018-05-08 14:40:03 -0400

    metadata

--- AB-generate-get-yen-reply.js
+++ AB-generate-get-yen-reply.js
@@ -5,7 +5,6 @@
 // @namespace   https://github.com/theothersophie/AB-generate-get-yen-reply/raw/master/AB-generate-get-yen-reply.js
 // @downloadURL https://github.com/theothersophie/AB-generate-get-yen-reply/raw/master/AB-generate-get-yen-reply.js
 // @require     https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js
-// @grant       GM.xmlHttpRequest
 // @version     2018.05.08
 // @run-at      document-end
 // ==/UserScript==
